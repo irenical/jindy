c38879a9ff0dd588dd8f9697127de44df89bdf59
Tiago Simão <tgsimao@impresa.pt>
2016-06-04 12:42:34 +0100

    renamed commons, added test project

--- jindy-apacheconfig-impl/src/main/java/org/irenical/jindy/commons/CommonsSimpleImpl.java
+++ jindy-apacheconfig-impl/src/main/java/org/irenical/jindy/commons/CommonsSimpleImpl.java
@@ -6,17 +6,28 @@ import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.configuration.SystemConfiguration;
 import org.irenical.jindy.ConfigFactory;
+import org.slf4j.LoggerFactory;
 
 public class CommonsSimpleImpl extends CommonsConfigurationBaseFactory {
 
   @Override
   protected Configuration createConfiguration(String name) throws ConfigurationException {
-    if(ConfigFactory.DEFAULT_CONFIG_NAME.equals(name)){
-      name="config.properties";
-    }
     CompositeConfiguration config = new CompositeConfiguration();
     config.addConfiguration(new SystemConfiguration());
-    config.addConfiguration(new PropertiesConfiguration(name));
+    try {
+      if(ConfigFactory.DEFAULT_CONFIG_NAME.equals(name)){
+        config.addConfiguration(new PropertiesConfiguration("config.properties"));
+      } else 
+        config.addConfiguration(new PropertiesConfiguration(name));
+    } catch (ConfigurationException e) {
+      if(!ConfigFactory.DEFAULT_CONFIG_NAME.equals(name)){
+        LoggerFactory.getLogger(CommonsSimpleImpl.class).info("No default properties file found (config.properties)");
+      } else {
+        throw e;
+      }
+    }
+    
+    
     return config;
   }
 
