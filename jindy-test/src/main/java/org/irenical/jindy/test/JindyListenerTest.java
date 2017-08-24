97e4fd20867a7d1e026a3638a1cd25840e597344
Tiago Simão <tiagosimao@gmail.com>
2017-08-24 18:46:27 +0100

    testing unlisten

--- jindy-test/src/main/java/org/irenical/jindy/test/JindyListenerTest.java
+++ jindy-test/src/main/java/org/irenical/jindy/test/JindyListenerTest.java
@@ -13,6 +13,23 @@ public class JindyListenerTest extends JindyBaseTest {
     });
     config.setProperty(P1, "CHANGED");
     Assert.assertEquals("CHANGED", gotValue[0]);
+    config.setProperty(P1, "CHANGEDAGAIN");
+    Assert.assertEquals("CHANGEDAGAIN", gotValue[0]);
+  }
+  
+  @Test
+  public void testUnlisten() {
+    String [] gotValue = new String[1];
+    String id = config.listen(P1, p -> {
+      gotValue[0]=config.getString(P1);
+    });
+    config.setProperty(P1, "CHANGED");
+    
+    config.unListen(id);
+    
+    config.setProperty(P1, "CHANGEDAGAIN");
+    
+    Assert.assertEquals("CHANGED", gotValue[0]);
   }
 
 }
